5fea74dc837caf8f87a2e92c9cecae15e8a0a27b
rockyae <2089431771@qq.com>
2023-12-28 01:07:48 +0800

    线程通信之中断标志位

--- src/main/java/com/example/demo/thread/MyThread.java
+++ src/main/java/com/example/demo/thread/MyThread.java
@@ -1,6 +1,7 @@
 package com.example.demo.thread;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class MyThread implements Runnable{
 
@@ -9,8 +10,13 @@ public class MyThread implements Runnable{
     @Override
     public void run() {
         try {
-           concurrentHashMap.put(3,"test3");
-            System.out.println(Thread.currentThread().getName()+"插入了一个元素");
+            int i = 0;
+            while (!Thread.currentThread().isInterrupted()){
+                i++;
+                if(i%1000 == 0)
+                    System.out.println(i);
+            }
+//            Thread.sleep(5000);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -18,19 +24,22 @@ public class MyThread implements Runnable{
 
 
     public static void main(String[] args) throws InterruptedException {
-//        Thread thread = new Thread(new MyThread());
-//        thread.start();
-//        thread.interrupt();
-//        Thread.sleep(1000);//sleep main从运行态变为阻塞态，MyThread获得时间片，就绪态变为运行态，打印"MyThead run"
-//        System.out.println("main run");
+        Thread thread = new Thread(new MyThread());
+        thread.setName("MyThead");
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+        thread.interrupt();//中断一个处于等待态的线程会抛出异常
+        //Thread.sleep(5000);
+        System.out.println(thread.isInterrupted());
+        System.out.println("main run");
 
         //模拟一个线程插入后正在扩容，另一个线程put会发生什么？
 
-        concurrentHashMap.put(1,"test");//主线程插入
-        concurrentHashMap.put(4,"test4");
-        Thread thread = new Thread(new MyThread(),"myThraed");
-        thread.start();
-        concurrentHashMap.put(2,"test2");
+//        concurrentHashMap.put(1,"test");//主线程插入
+//        concurrentHashMap.put(4,"test4");
+//        Thread thread = new Thread(new MyThread(),"myThraed");
+//        thread.start();
+//        concurrentHashMap.put(2,"test2");
 
     }
 }
